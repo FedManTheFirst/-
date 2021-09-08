8edf8cdb7f4e7c874e90f6e080cc274e34a6b70c
FedMan(Федя) <84412996+FedManTheFirst@users.noreply.github.com>
2021-09-08 17:28:12 +0300

    Update Program.cs

--- Checking_conversion.Pyrshiev/Checking_conversion.Pyrshiev/Program.cs
+++ Checking_conversion.Pyrshiev/Checking_conversion.Pyrshiev/Program.cs
@@ -8,7 +8,7 @@ namespace Checking_conversion.Pyrshiev
         {
             Int16 i16 = 1; Int32 i32 = 1; double db = 1;
             i16 = 132;
-            i16 = db;
+            //i16 = db; - db more then short
             i32 = i16;
             //i32 = db; - db more then int
 
