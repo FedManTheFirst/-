ddfe45ef08f1ce7a87812d93e4baede7e5a970fa
FedMan(Федя) <84412996+FedManTheFirst@users.noreply.github.com>
2021-09-08 17:15:31 +0300

    Add files via upload

---
+++ CreateStruct.Pyrshiev/CreateStruct.Pyrshiev/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CreateStruct.Pyrshiev
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person p = new Person("Fedor", "Pyrshiev", 19);
+            Console.WriteLine(p);
+        }
+        struct Person
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+            public Person(string _firstName, string _lastName, int _age)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                age = _age;
+            }
+            public override string ToString()
+            {
+                return firstName + " " + lastName + ", age " + age;
+            }
+
+        }
+    }
+}
