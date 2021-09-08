2de1b78cc3cd93a6ff0d329b29e4744990a66633
FedMan(Федя) <84412996+FedManTheFirst@users.noreply.github.com>
2021-09-08 17:24:30 +0300

    Add files via upload

---
+++ Checking_conversion.Pyrshiev/Checking_conversion.Pyrshiev/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Checking_conversion.Pyrshiev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1; Int32 i32 = 1; double db = 1;
+            i16 = 132;
+            i16 = db;
+            i32 = i16;
+            //i32 = db; - db more then int
+
+        }
+    }
+}
