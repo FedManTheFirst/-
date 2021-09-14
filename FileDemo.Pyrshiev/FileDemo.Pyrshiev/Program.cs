8ce71c7b476df92f07cfc0b0e8165c1967b98016
FedMan(Федя) <84412996+FedManTheFirst@users.noreply.github.com>
2021-09-14 14:22:07 +0300

    Add files via upload

---
+++ FileDemo.Pyrshiev/FileDemo.Pyrshiev/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace FileDemo.Pyrshiev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 lesson
+            StreamWriter writer = File.CreateText(@"c:\Проба пера.txt");
+            writer.WriteLine("Это Федя балуется, чтобы разобраться в функионале");
+            writer.WriteLine(":3");
+            writer.Close();
+            // 2 lesson
+            StreamReader reader = File.OpenText(@"c:\Проба пера.txt");
+            string contents = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(contents);
+        }
+    }
+}
