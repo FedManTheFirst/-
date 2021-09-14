94944598ab5cdc87b31125fba8602e62895fe2e5
FedMan(Федя) <84412996+FedManTheFirst@users.noreply.github.com>
2021-09-14 14:43:04 +0300

    Add files via upload

---
+++ CompressionDemo.Pyrshiev/CompressionDemo.Pyrshiev/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+namespace CompressionDemo.Pyrshiev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CompressFile(@"c:\boot.ini", @"с:\boot.ini.gz");
+            UncompressFile(@"c:\boot.ini.gz", @"c:\boot.ini.test");
+        }
+      /* static void CompressFile(string inFilename, string outFilename)
+        {
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.Create(outFilename);
+            GZipStream compStream = new GZipStream(destFile, CompressionMode.Compress);
+            int theByte = sourceFile.ReadByte();
+            while (theByte != -1)
+            {
+                compStream.WriteByte((byte)theByte); theByte = sourceFile.ReadByte();
+            }
+        }*/
+        static void UncompressFile(string inFilename, string outFilename)
+        {
+            string inFilename1 = @"c:\boot.ini";
+            FileStream sourceFile = File.OpenRead(outFilename);
+            FileStream destFile = File.Create(inFilename1);
+            GZipStream compStream =
+            new GZipStream(sourceFile, CompressionMode.Decompress);
+            int theByte = compStream.ReadByte();
+            while (theByte != -1)
+            {
+                destFile.WriteByte((byte)theByte); theByte = compStream.ReadByte();
+            }
+
+
+            compStream.Close();
+        }
+
+
+    }
+}
