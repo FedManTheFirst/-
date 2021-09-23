246df54a88d4d4c1bc2ff95adb3bbb57dd23dec4
FedMan(Федя) <84412996+FedManTheFirst@users.noreply.github.com>
2021-09-23 16:53:24 +0300

    Add files via upload

---
+++ LookupCollection.Pyrshiev/LookupCollection.Pyrshiev/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace LookupCollection.Pyrshiev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создать Словарь, нечувствительный к регистру 
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+            // Добавить несколько элементов
+            list["Estados Unidos"] = "United States of America";
+            list["Canada"] = "Canada";
+            list["Espana"] = "Spain";
+            //  Показать результаты
+            Console.WriteLine(list["espana"]);
+            Console.WriteLine(list["CANADA"]);
+            Console.Read();
+        }
+    }
+
+}
