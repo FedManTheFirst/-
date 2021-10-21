a22c37db22e792e46528b6f99f510e0bc5c56486
FedMan(Федя) <84412996+FedManTheFirst@users.noreply.github.com>
2021-10-21 16:56:25 +0300

    Add files via upload

---
+++ SingleInstance.Pyrshiev/SingleInstance.Pyrshiev/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Mutex oneMutex = null;
+        const String MutexName = "RUNMEONLYONCE";
+        try // Пытаемся открыть Mutex
+        {
+            oneMutex = Mutex.OpenExisting(MutexName);
+        }
+        catch (WaitHandleCannotBeOpenedException)
+        {
+            // He можемоткрыть Mutex, потому что он не существует
+        }
+        if (oneMutex== null)
+        {
+            oneMutex = new Mutex(true, MutexName);
+        }
+        else
+        {
+            // Закрываем Mutexи выходим из приложения,
+            // так как разрешается запуск только одного его экземпляра
+            oneMutex.Close();
+            return;
+        }
+        Console.WriteLine("Our Application");
+        Console.Read();
+}
+}
+
